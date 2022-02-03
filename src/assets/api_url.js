9e616dcc6e573ce8ea7480bd20d20400397df573
Tim Coggins <timcoggins@gmail.com>
2022-02-03 12:47:55 +0100

    back to http

--- src/assets/api_url.js
+++ src/assets/api_url.js
@@ -1,5 +1,5 @@
 //const API_URL = 'http://192.168.0.226:8000'
 
-const API_URL = 'https://api.unethicalmachines.studio:8000'
+const API_URL = 'http://api.unethicalmachines.studio:8000'
 
 export default API_URL
